2a290f52716d2d895d08c0013ab31df565bef154
Corey Whiting <correll.whiting@gmail.com>
2016-06-06 10:34:16 -0400

    z-scores in baseball matchup

--- app/controllers/baseball_matchup_controller.rb
+++ app/controllers/baseball_matchup_controller.rb
@@ -17,12 +17,12 @@ class BaseballMatchupController < ApplicationController
               :whip,
               :k9 ]
 
-    #calc
+    #CALCULATIONS
     @members = Member.all.order(espn_id: :desc)
-    @team_averages = Array.new
-    @stat_means = {}
-    @stat_stddev = {}
 
+    #TEAM AVERAGES
+    #weekly averages for a team over start/end week parameters
+    @team_averages = Array.new
     @members.each do |m|
       @team_averages[m.espn_id] = {
           :runs => BaseballMatchup.where(espn_id: m.espn_id, week: @first_week..@last_week).average(:runs),
@@ -39,6 +39,58 @@ class BaseballMatchupController < ApplicationController
           :k9 => BaseballMatchup.where(espn_id: m.espn_id, week: @first_week..@last_week).average(:k9)
       }
     end
+
+    #STAT MEAN/STDEV
+    #for all members, find mean and st dev of a stat over a week. will be used for z-scores and matchups
+    @stat_means = {}
+    @stat_stddev = {}
+
+    stats.each do |s|
+      @stat_means[s] = BaseballMatchup.where(week: @first_week..@last_week).average(s)
+
+      squared_numbers_minus_means = Array.new
+      numbers = Array.new
+
+      #get all average of stat into an array
+      @team_averages.each do |t|
+        if t != nil
+          numbers << t[s]
+        end
+      end
+
+      numbers.each do |n|
+        squared_numbers_minus_means << (n.to_f - @stat_means[s].to_f) * (n.to_f - @stat_means[s].to_f)
+      end
+      mean_squared_diffs = squared_numbers_minus_means.inject{ |sum, el| sum + el }.to_f / squared_numbers_minus_means.size
+
+      @stat_stddev[s] = Math.sqrt(mean_squared_diffs)
+    end
+
+    #ZSCORES
+    #how far away from the mean is each member?
+    #winners and losers represent a team > or < .5 standard deviations from the mean
+    @zscores = Hash.new{|h, k| h[k] = []}
+
+    @members.each do |m|
+      @zscores[m.espn_id][12] = 0 #winners
+      @zscores[m.espn_id][13] = 0 #losers
+
+      stats.each_with_index do |stat,i|
+        @zscores[m.espn_id][i] = (@team_averages[m.espn_id][stat].to_f - @stat_means[stat].to_f) / @stat_stddev[stat].to_f
+
+        if i == 9 || i == 10
+          @zscores[m.espn_id][i] = @zscores[m.espn_id][i] * -1
+        end
+
+        if (@zscores[m.espn_id][i] > 0.5)
+          @zscores[m.espn_id][12] = @zscores[m.espn_id][12] + 1 #winners
+        elsif (@zscores[m.espn_id][i] < -0.5)
+          @zscores[m.espn_id][13] = @zscores[m.espn_id][13] + 1 #losers
+        end
+
+      end
+    end
+
   end
 
   private
--- spec/features/show_specific_weeks_baseball_matchup_spec.rb
+++ spec/features/show_specific_weeks_baseball_matchup_spec.rb
@@ -6,7 +6,18 @@ feature "view specfic weeks in baseball matchup index" do
     visit baseball_matchup_index_path
 
     expect(page).to have_content("team averages")
-    expect(page).to have_content("z scores")
+    expect(page).to have_content("z-scores")
+  end
+
+  scenario "with 2 to 4 week parameters" do
+    visit baseball_matchup_index_path
+
+    fill_in('week_range_first', :with => '2')
+    fill_in('week_range_last', :with => '4')
+    click_button('week_range_submit')
+
+    expect(page).to have_content("team averages")
+    expect(page).to have_content("z-scores")
   end
 
 end
